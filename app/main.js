5d436d5a6363d80c5d3079a91d8f86c799740f12
Shazeb Qadir <sqadir@ubermedia.com>
2015-06-17 15:31:44 -0700

    test out angularfire stuff

--- app/main.js
+++ app/main.js
@@ -1,9 +1,25 @@
-var site = angular.module('arabicSite', []);
+var site = angular.module('arabicSite', ['firebase']);
 
-site.controller('rootCtrl', function($scope) {
+site.controller('rootCtrl', function($scope, $firebaseObject, $firebaseArray) {
 
     $scope.name = 'Shazeb Qadir';
 
+    var ref = new Firebase("https://sizzling-inferno-9713.firebaseio.com/data");
+
+    // download the data into a local object
+    var syncObject = $firebaseObject(ref);
+
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    syncObject.$bindTo($scope, "data")
+
+    $scope.messages = $firebaseArray(ref);
+
+    $scope.addMessage = function() {
+        $scope.messages.$add({
+            text: $scope.newMessageText
+        });
+    }
 })
 
 
