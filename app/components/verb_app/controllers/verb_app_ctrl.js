1fd0a0f5bf48a7876517632a46ad9af685ab47a1
Shazeb Qadir <sqadir@ubermedia.com>
2015-08-13 20:07:28 -0700

    add conjugator to ctrl

--- app/components/verb_app/controllers/verb_app_ctrl.js
+++ app/components/verb_app/controllers/verb_app_ctrl.js
@@ -1,11 +1,14 @@
 var verbApp = angular.module('verbApp');
 
-verbApp.controller('verbAppCtrl', function($scope, conjugator, hamzatedWord, helperData, menuOptions) {
+verbApp.controller('verbAppCtrl', function($scope, conjugator, helperData, menuOptions, verbs) {
 
     $scope.helperData = helperData;
 
     $scope.menuOptions = menuOptions;
 
+    $scope.verbs = verbs;
+
+    $scope.conjugator = conjugator;
 
 
 })
--- build.js
+++ build.js
@@ -51,12 +51,15 @@ verbApp.controller('conjugatorCtrl', function($scope, conjugator, hamzatedWord, 
 })
 ;var verbApp = angular.module('verbApp');
 
-verbApp.controller('verbAppCtrl', function($scope, conjugator, hamzatedWord, helperData, menuOptions) {
+verbApp.controller('verbAppCtrl', function($scope, conjugator, helperData, menuOptions, verbs) {
 
     $scope.helperData = helperData;
 
     $scope.menuOptions = menuOptions;
 
+    $scope.verbs = verbs;
+
+    $scope.conjugator = conjugator;
 
 
 })
