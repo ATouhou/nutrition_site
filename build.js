f5053d133ec850ab3ea50796c71e81063bd6197b
Shazeb Qadir <sqadir@ubermedia.com>
2015-08-23 21:02:39 -0700

    updates

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-var app = angular.module('arabicSite', ['ui.router', 'verbApp']);
+var app = angular.module('arabicSite', ['ui.router', 'ngAnimate', 'verbApp']);
 
 app.run(function ($rootScope) {
     $rootScope._ = window._;
