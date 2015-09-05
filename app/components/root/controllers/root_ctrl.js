3cd45b92258bc97f40c44edebdc1fe93a920e8ec
Shazeb Qadir <sqadir@ubermedia.com>
2015-09-04 23:43:27 -0700

    updates

--- app/components/root/controllers/root_ctrl.js
+++ app/components/root/controllers/root_ctrl.js
@@ -3,7 +3,8 @@ var app = angular.module('arabicSite');
 app.controller('rootCtrl', function($scope) {
     $scope._ = _;
 
-    console.log('helloo');
+    $scope.globals = {};
+    $scope.globals.count = 0;
 })
 
 String.prototype.capitalize = function() {
