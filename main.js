7b6ba5d433d0a1400087b641151d5e062d3dda0e
Shazeb Qadir <sqadir@ubermedia.com>
2015-09-06 17:58:22 -0700

    updates

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ app.config(function($urlRouterProvider) {
     // This redirects to the conjugator app when the base url is entered
     $urlRouterProvider.when('', '/conjugation_practice');
 })
+
+// Check if a new cache is available on page load.
+// This is needed because when I do git push origin master and user accesses page, the old cached page is served and the new update is not
+// reflected.
+window.addEventListener('load', function(e) {
+
+    window.applicationCache.addEventListener('updateready', function(e) {
+        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+            // Browser downloaded a new app cache.
+            // Swap it in and reload the page to get the new hotness.
+            window.applicationCache.swapCache();
+            if (confirm('A new version of this site is available. Load it?')) {
+                window.location.reload();
+            }
+        } else {
+            // Manifest didn't changed. Nothing new to server.
+        }
+    }, false);
+
+}, false);
